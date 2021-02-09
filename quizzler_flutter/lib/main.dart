b70e768b35629eaa654b18c277cbc3a798cf0967
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-02-09 16:44:18 +0500

    Lecture 9 & 10
    Complete quizzler app with the understanding of four pillers of OOP.

--- quizzler_flutter/lib/main.dart
+++ quizzler_flutter/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() {
   runApp(Quizzler());
@@ -27,6 +30,32 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  void displayAnswers(bool userSelectedAnswer) {
+    bool correctAnswer = quizBrain.getAnswerVal();
+
+    if (userSelectedAnswer == correctAnswer) {
+      scoreKeeper.add(
+        Icon(
+          Icons.check,
+          color: Colors.green,
+        ),
+      );
+    } else {
+      scoreKeeper.add(
+        Icon(
+          Icons.close,
+          color: Colors.red,
+        ),
+      );
+    }
+
+    setState(() {
+      quizBrain.getNextQuestion();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -39,7 +68,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question will be displayed.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -55,7 +84,7 @@ class _QuizPageState extends State<QuizPage> {
             child: FlatButton(
               color: Colors.green,
               onPressed: () {
-                // TODO: The user picked true.
+                displayAnswers(true);
               },
               child: Text(
                 'True',
@@ -73,7 +102,7 @@ class _QuizPageState extends State<QuizPage> {
             child: FlatButton(
               color: Colors.red,
               onPressed: () {
-                // TODO: The user picked false.
+                displayAnswers(false);
               },
               child: Text(
                 'False',
@@ -85,8 +114,16 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        // TODO: Add a Row here for score keeping
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
 }
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
