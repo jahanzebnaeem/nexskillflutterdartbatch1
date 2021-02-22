fdccd605610f502d0485d2fd2879dcdb3aca5b4d
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-02-22 17:12:05 +0500

    Lecture 11
    New BMI Calculator app start with the customisation and initial screen design.

---
+++ bmi_calculator_flutter/lib/input_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1c1e32);
+const bottomContainerColour = Color(0xFFea1555);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColour,
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColour,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(
+                colour: activeCardColour,
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColour,
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColour,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: bottomContainerColour,
+              margin: EdgeInsets.only(top: 10.0),
+              height: bottomContainerHeight,
+              width: double.infinity,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
---
+++ bmi_calculator_flutter/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() {
+  runApp(BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF090d21),
+        scaffoldBackgroundColor: Color(0xFF090d21),
+      ),
+      home: InputPage(),
+    );
+  }
+}
